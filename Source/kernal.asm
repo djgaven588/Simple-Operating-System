c5fa1a4c4818d3a4454b0327701ecb2e06b0035a
Cj Herman <cjherman588@gmail.com>
2019-05-16 22:22:41 -0700

    Idk wtf is wrong....

--- Source/kernal.asm
+++ Source/kernal.asm
@@ -1,12 +1,12 @@
 org 0x7E00
 BITS 16
 
-mov si, TEXT_loadSector	    ; Put 'text_string' reference into si register
+mov si, TEXT_success    ; Put 'text_string' reference into si register
 call print_string_16    ; Call print_string, coming back here after
 
 jmp $			        ; Jump to here, from here. Infinite loop.
 
-TEXT_loadSector db 'This is my first OS, and it works!', 0x0D, 0xA, 0
+TEXT_success db 'This is my first OS, and it works!', 0x0D, 0xA, 0
 
 %include "print_string_16.asm"
 
