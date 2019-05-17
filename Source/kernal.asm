3bdba20372f76ab400502a0f4799a7f99b8ae608
Cj Herman <cjherman588@gmail.com>
2019-05-17 14:41:29 -0700

    Removed that stuff. VirtualBox does the job.

--- Source/kernal.asm
+++ Source/kernal.asm
@@ -1,10 +1,10 @@
 org 0x7E00
 BITS 16
 
-mov si, text_string	; Put 'text_string' reference into si register
-call print_string_16; Call print_string, coming back here after
+mov si, text_string	    ; Put 'text_string' reference into si register
+call print_string_16    ; Call print_string, coming back here after
 
-jmp $			    ; Jump to here, from here. Infinite loop.
+jmp $			        ; Jump to here, from here. Infinite loop.
 
 text_string db 'This is my first OS, and it works!', 0x0D, 0xA, 0
 
--- qemu/gnu-mcu-eclipse/scripts/git-clone.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-rm -rf ~/Downloads/qemu-build.git
-git clone --recurse-submodules https://github.com/gnu-mcu-eclipse/qemu-build.git ~/Downloads/qemu-build.git
